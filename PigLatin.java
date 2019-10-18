b67ad3299c185dbc55391d184718ef959a7f5c3f
Ale2006 <54778764+Ale2006@users.noreply.github.com>
2019-10-18 18:59:04 -0300

    Update PigLatin.java

--- PigLatin.java
+++ PigLatin.java
@@ -2,6 +2,7 @@ package piglatin;
 
 import java.util.Scanner;
 
+
 public class PigLatin {
 
    
@@ -12,71 +13,50 @@ public class PigLatin {
 		System.err.println("Ingrese el texto en ingles a traducir (vacio para terminar): ");
 
 		String cadena;
+                
 		while ((cadena= scn.nextLine())!=null && !cadena.isEmpty()) { 
-			mostrarArray( traducir(cadena) );
-			System.err.println("Ingrese el texto en ingles a traducir (vacio para terminar): ");
+			
+                    mostrarArray( separarPalabras(cadena.toLowerCase()) );
+		    System.err.println("Ingrese el texto en ingles a traducir (vacio para terminar): ");
 		}
 
-		mostrarArray( traducir("Thanks for using the Pig Latin translator!") );
+	 	mostrarArray( "Thanks for using the Pig Latin translator!" );
 	}
 
 
 
+    
+public static String separarPalabras(String cadena){
+String paltraducidas="";
+String sololetras= "^[a-zA-Z ]*$";
 
-	public static String[] palabras(String cadena) {
-	
-                String [] arrayCadena= cadena.split(" ");
-		return llenarArray(arrayCadena, cadena);
-	}
+    for (String palabra : cadena.split(" ")) {
+        
+        String palunamenos=palabra.substring(0,(palabra.length()-1));
+        String ultimocaracter=palabra.substring((palabra.length()-1),(palabra.length()));
+        
+        if (!palabra.matches("^[a-zA-Z ]*$")) {
+        
+            int pos= palabra.indexOf(','|'!'|'?'|'.'|':'|';'|'-'|'_');
+        paltraducidas+= traducirPalabra(palunamenos)+ ultimocaracter + " ";
+        }
+        
+        else {
+        paltraducidas+= traducirPalabra(palabra) + " ";
+        }
+    }
 
-	public static String[] traducir(String cadena) {
-		return traducir( palabras(cadena) );
-	}
 
-	public static String[] traducir(String[] palabras){
-		//PodemosAprender: esta funcion es un caso particular de Map, recorre una estructura de datos y le aplica una func a cada elemento
-		//SEE: https://dzone.com/articles/how-to-use-map-filter-collect-of-stream-in-java-8
 
-		//TODO: como traducir sin perder los signos de puntuacion?
-		String[] r= new String[ palabras.length ];
-		for (int i = 0; i < palabras.length ; i++) {
-                    
-                    r[i]= traducirUna( palabras[i] );
-                    
-                    }
-		return r;
-	}
+return paltraducidas;
+}
 
-	public static String traducirUna(String palabra) {
-		//PodemosAprender: conviene hacer funciones lo mas chiquitas y especializadas que puedas, en este problema la unidad es convertir UNA palabra
-		String palabraPigLatin= "ERROR!"; //DFLT
-
-		if (palabra.length()<2) { return palabra; } //A: si tiene menos de dos letras, la dejamos igual
-
-		boolean isVocal1raLetra= isVocal(palabra.substring(0,1));   
-		boolean isVocal2daLetra= isVocal(palabra.substring(1,2));
-		
-      if(isVocal1raLetra){ //A: empieza con vocal
-		   palabraPigLatin= palabra+"yay";
-		}
-		else if (!isVocal1raLetra && !isVocal2daLetra){ //A: empieza con dos consonantes  
-			palabraPigLatin= palabra.substring(2) + palabra.substring(0,2) + "ay";
-		}
-		else if (!isVocal1raLetra && isVocal2daLetra) { //A: empieza con consonante (falso el primer if) pero la segunda es vocal (falso segundo if) 
-			palabraPigLatin= palabra.substring(1) + palabra.substring(0,1) + "ay";
-		}
-		else {
-			//TODO: error, puede pasar? como se maneja?
-		}
-		return palabraPigLatin;
-	}
-
-	public static String traducirUnaConRegex(String cadena){
-
-		String empiezaVocal="^[aeiou]{1}.*";
-		String dosConsonantes="^[^aeiou]{2}.*";
-
-		if (cadena.matches(empiezaVocal)){ 
+public static String traducirPalabra(String cadena){
+    String empiezaVocal="^[aeiou]{1}.*";
+    String dosConsonantes="^[^aeiou]{2}.*";
+    
+    
+                if (cadena.matches(empiezaVocal)){ 
 			cadena=cadena + "yay";
 
 		}
@@ -90,49 +70,16 @@ public class PigLatin {
 		return cadena;
 	}
 
-	public static String[] llenarArray(String[] arrayCadena, String cadena){
-		int j=0;
-		cadena= cadena.toLowerCase();
-                
-
-		for (int i= 0; i < arrayCadena.length; i++) {
-			arrayCadena[i]="";
-		}
-
-		for (char x : cadena.toCharArray()) {
-			
-                        if (isLetra(x)){  //|| x== ',') { //esto esta a prueba
-				arrayCadena[j]+=x;   
-			}
-			
-                        else if(x== ' '){
-			    j++;
-			}
-		}
-		return arrayCadena;
-	}
-
-
+	
 	public static boolean isLetra(char c) {
 		return Character.isLetter(c); //SEE: https://docs.oracle.com/javase/7/docs/api/java/lang/Character.html#isAlphabetic(int)
 	}
 
-	public static boolean isVocal(String x){
-	        String empiezaVocal="^[aeiou]{1}.*";
-		
-		if (x.matches(empiezaVocal)) 
-			return true;
-			
-            
-                return false;
-	}
-        
-        
-	public static void mostrarArray(String[] palabras){
-		for (String palabra : palabras) {
-			System.out.print(palabra + " ");
-		}
-		System.out.println("");
+	        
+	public static void mostrarArray(String palabras){
+		          
+            System.out.println(palabras);
+	    System.out.println("");
 	}
 
 }
