2c571b35f39506d3095375fdcad14f1c45f2d2d0
Ale2006 <54778764+Ale2006@users.noreply.github.com>
2019-10-23 18:15:01 -0300

    Clase extendida

--- APigLatin.java
+++ APigLatin.java
@@ -1,22 +1,23 @@
 package piglatin;
 
-public class APigLatin {
-    public String Traducir(String cadena){
+public class APigLatin extends AOtroIdioma{
+    
+    public String traducir(String cadena){
     String empiezaVocal="^[aeiou]{1}.*";
     String dosConsonantes="^[^aeiou]{2}.*";
     
-    
-                if (cadena.matches(empiezaVocal)){ 
+       if (cadena.matches(empiezaVocal) && cadena.length()>1){ 
 			cadena=cadena + "yay";
 
-		}
-		else if (cadena.matches(dosConsonantes)) {
-			cadena= cadena.substring(2) + cadena.substring(0,2)+"ay";
+	}
+      
+	else if (cadena.matches(dosConsonantes)) {
+	    cadena= cadena.substring(2) + cadena.substring(0,2)+"ay";
 
-		}
-		else{
-			cadena= cadena.substring(1) + cadena.substring(0,1)+"ay";
-		}
+	}
+        else if(!cadena.matches(empiezaVocal)){
+	    cadena= cadena.substring(1) + cadena.substring(0,1)+"ay";
+	}
 		return cadena;
 	}
 }
