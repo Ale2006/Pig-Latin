745f484831ca37eb08872f7369a567736c2e376b
Ale2006 <54778764+Ale2006@users.noreply.github.com>
2019-10-23 18:15:24 -0300

    Add files via upload

---
+++ AOtroIdioma.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package piglatin;
+
+/**
+ *
+ * @author a
+ */
+public class AOtroIdioma {
+    
+    public String traducir(String cadena){
+    
+        
+    return cadena;
+    }
+}
